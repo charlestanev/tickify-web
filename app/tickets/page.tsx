3ca8aeff3278175805346f36e8c15aaafb55b1de
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-10-23 10:29:16 +0300

    Refactor Tickets component to fetch and log tickets using Prisma

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import prisma from '@/prisma/db'
+import { log } from 'console'
 
-const Tickets = () => {
+const Tickets = async () => {
+    const tickets = await prisma.ticket.findMany()
+    console.log(tickets);
+
+
     return (
         <div>Tickets</div>
     )
