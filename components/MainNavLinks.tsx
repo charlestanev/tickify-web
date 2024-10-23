552a761631c1987abb961d317f602fc457252160
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-10-23 09:40:44 +0300

    Refactor MainNavLinks component and add current path highlighting

--- components/MainNavLinks.tsx
+++ components/MainNavLinks.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React, { use } from 'react'
 
 const MainNavLinks = () => {
@@ -10,13 +11,15 @@ const MainNavLinks = () => {
         { href: '/users', label: 'Users' },
     ];
 
+    const currentPath = usePathname();
 
     return (
         <div className="flex items-center gap-2">
             {links.map(link => (
                 <Link
                     href={link.href}
-                    className='navbar-link'>
+                    className={`navbar-link ${currentPath === link.href ? 'cursor-default text-prymary/10 hover:text-primary/60' : ''}`}
+                    key={link.label}>
                     {link.label}
                 </Link>
             ))}
