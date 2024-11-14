dbe98737afb416e67f9c8c80e80b5ddcfc29bfc4
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-14 15:36:46 +0200

    Add form handling and validation to TicketForm component with Zod integration

--- components/TicketForm.tsx
+++ components/TicketForm.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from 'react';
-import { Form } from './ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import { ticketSchema } from '@/ValidationSchemas/tickets';
 import { z } from 'zod';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { ticketSchema } from '@/lib/schema';
+import { Input } from './ui/input';
+import "easymde/dist/easymde.min.css";
+import SimpleMDE from "react-simplemde-editor";
 
 type TicketFormData = z.infer<typeof ticketSchema>;
 
@@ -13,9 +16,34 @@ const TicketForm = () => {
         resolver: zodResolver(ticketSchema),
     });
 
+    async function onSubmit(values: z.infer<typeof ticketSchema>) {
+        console.log(values);
+
+    }
+
     return (
         <div>
-            <Form {...form} />
+            <Form {...form} >
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Ticket Title</FormLabel>
+                                <FormControl>
+                                    <Input placeholder='Ticket Title' {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )} />
+                    <Controller
+                        name="description"
+                        control={form.control}
+                        render={({ field }) => (
+                            <SimpleMDE placeholder='Ticket Description' {...field} />
+                        )} />
+                </form>
+            </Form>
         </div>
     );
 };
