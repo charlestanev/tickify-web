a3d37c1853a38f82ef2fd7ef122f5875b10f37e9
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-11-14 14:27:19 +0200

    Add TicketForm component and integrate it into NewTicket page

---
+++ app/tickets/new/page.tsx
@@ -0,0 +1,10 @@
+import TicketForm from '@/components/TicketForm'
+import React from 'react'
+
+const NewTicket = () => {
+    return (
+        <TicketForm />
+    )
+}
+
+export default NewTicket
---
+++ components/TicketForm.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const TicketForm = () => {
+    return (
+        <div>TicketForm</div>
+    )
+}
+
+export default TicketForm
